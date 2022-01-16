eec1cb1883af4eb7c84d0e5cc732ad4937a6d59d
YDulanjani <ydulanjani@gmail.com>
2022-01-16 22:11:38 +0530

    Revert "adding files related to CRUD operations and test suites"

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM maven:3.3.9-jdk-8-alpine
-WORKDIR /opt
-ENV PORT 3000
-EXPOSE 3000
-COPY target/*.jar /opt/app.jar
-ENTRYPOINT exec java $JAVA_OPTS -jar app.jar
-
-
-
--- src/main/java/com/yamuna/crypto/repository/TransactionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.yamuna.crypto.repository;
-
-import com.yamuna.crypto.dao.Transactions;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRepository extends JpaRepository<Transactions, Long > {
-}
